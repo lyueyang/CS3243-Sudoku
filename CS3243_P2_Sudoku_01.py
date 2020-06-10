00ec7ea5ae554e07d407a3529890135881c7bb10
Yue Yang <lyueyang@u.nus.edu>
2020-06-10 19:33:02 +0800

    Big Optimisation Update
    - Assigned keys are now held in a dictionary instead of a list
    - Deepcopy avoided as much as possible

--- CS3243_P2_Sudoku_01.py
+++ CS3243_P2_Sudoku_01.py
@@ -7,98 +7,118 @@ import copy
 
 # Running script: given code can be run with the command:
 # python file.py, ./path/to/init_state.txt ./output/output.txt
-class Node:
-    def __init__(self, list_of_variables):
-        list_of_variables.sort(key=lambda x: x.domain_size, reverse=False)
 
-        self.variables = list_of_variables
-        # self.puzzle = grid
+
+class Node:
+    def __init__(self, unassigned_vars, assigned_vars):
+        unassigned_vars.sort(key=lambda x: len(x.domain), reverse=False)
+        self.variables = unassigned_vars
+        self.assigned_vars = assigned_vars
 
 
 class Cell:
-    domain = set()
-    cell_id = -1
-    domain_size = -1
-    assigned_number = -1
-    row_num = -1
-    col_num = -1
-    box_num = -1
-
     def __init__(self, cell_id, dom, new_number):
         self.domain = dom
         self.cell_id = cell_id
-        self.domain_size = sys.maxint if len(dom) == 0 else len(dom)
         self.assigned_number = new_number
-
-        working_id = cell_id - 1
-        row_num = working_id // 9
-        col_num = working_id % 9
-
-        # these numbers start from index 1
-        self.row_num = row_num + 1
-        self.col_num = col_num + 1
-        self.box_num = (((self.row_num - 1) // 3)) * 3 + (((self.col_num - 1) // 3) + 1)
-
-    def __str__(self):
-        return "domain size: {0}, ID: {1}, domain: {2}, assigned number: {3}, coordinates: ({4}, {5}, {6})".format(
-            self.domain_size, self.cell_id, self.domain, self.assigned_number, self.row_num, self.col_num, self.box_num)
 
     def assign_number(self, new_number):
-        self.assigned_number = new_number
-
-        # empty the domain, no longer need to maintain domain after number is assigned
-        self.domain = set()
-        self.domain_size = sys.maxint
+        new_cell = Cell(self.cell_id, set(), new_number)
+        new_cell.remove_domain()
+        return new_cell
 
     def remove_number(self, current_number):
         if current_number in self.domain:
             self.domain.remove(current_number)
-            self.domain_size = sys.maxint if len(self.domain) == 0 else len(self.domain)
+
+    def remove_domain(self):
+        del self.domain
+
+    def get_row(self):
+        return (self.cell_id - 1) // 9 + 1
+
+    def get_col(self):
+        return (self.cell_id - 1) % 9 + 1
+
+    def get_box(self):
+        return ((self.get_row() - 1) // 3) * 3 + (((self.get_col() - 1) // 3) + 1)
 
 
 def init_solve(puzzle_grid):
-    l = []
-    i = 1
+    unassigned_vars = []
+    assigned_vars = dict()
+    cell_id = 1
 
     for index in xrange(0, len(puzzle_grid)):
         for index2 in xrange(0, len(puzzle_grid[index])):
-            domain = set(xrange(1, 10))
 
             cell_integer = puzzle_grid[index][index2]
             if cell_integer != 0:
-                domain = set()
+                assigned_vars[cell_id] = cell_integer
+            else:
+                domain = set(xrange(1, 10))
+                unassigned_vars.append(Cell(cell_id, domain, cell_integer))
 
-            current_cell = Cell(i, domain, cell_integer)
-            l.append(current_cell)
-            i += 1
+            cell_id += 1
 
-    return Node(domain_update(l))
+    return init_domain_update(unassigned_vars, assigned_vars)
 
 
-def domain_update(my_list):
-    working_list = copy.deepcopy(my_list)
+def init_domain_update(unassigned_vars, assigned_vars):
+    for init_var in assigned_vars.items():
+        row_num = (init_var[0] - 1) // 9 + 1
+        col_num = (init_var[0] - 1) % 9 + 1
+        box_num = ((row_num - 1) // 3) * 3 + (((col_num - 1) // 3) + 1)
 
-    for index in xrange(0, len(working_list)):
-        # if assigned number is 0 don't need to try to change anything else
-        if working_list[index].assigned_number == 0:
-            continue
-        else:
-            current_element = working_list[index]
+        for index_2 in xrange(0, len(unassigned_vars)):
+            unassigned_row_num = unassigned_vars[index_2].get_row()
+            unassigned_col_num = unassigned_vars[index_2].get_col()
+            unassigned_box_num = unassigned_vars[index_2].get_box()
 
-            for index_2 in xrange(0, len(working_list)):
+            if (row_num == unassigned_row_num
+                    or col_num == unassigned_col_num
+                    or box_num == unassigned_box_num):
+                unassigned_vars[index_2].remove_number(init_var[1])
 
-                # skip number if current number is not 0
-                # skip number of it is itself
-                if working_list[index_2].assigned_number > 0 or index == index_2:
-                    continue
+    return Node(unassigned_vars, assigned_vars)
+
+
+def domain_update(unassigned_vars, assigned_vars, number):
+    working_list = []
+    new_assigned_vars = assigned_vars.copy()
+
+    var = unassigned_vars[0].assign_number(number)
+    # new_assigned_vars.append(var)
+    new_assigned_vars[var.cell_id] = number
+
+    for index in xrange(1, len(unassigned_vars)):
+        row_num = var.get_row()
+        col_num = var.get_col()
+        box_num = var.get_box()
+
+        if (row_num == unassigned_vars[index].get_row()
+                or col_num == unassigned_vars[index].get_col()
+                or box_num == unassigned_vars[index].get_box()):
+
+            if len(unassigned_vars[index].domain) > 0:
+                working_domain = set()
+
+                if number in unassigned_vars[index].domain:
+                    for n in unassigned_vars[index].domain:
+                        if number != n:
+                            working_domain.add(n)
                 else:
-                    if (current_element.row_num == working_list[index_2].row_num
-                            or current_element.col_num == working_list[index_2].col_num
-                            or current_element.box_num == working_list[index_2].box_num):
+                    for n in unassigned_vars[index].domain:
+                        working_domain.add(n)
 
-                        working_list[index_2].remove_number(current_element.assigned_number)
+            else:
+                working_domain = set()
+            working_number = Cell(unassigned_vars[index].cell_id, working_domain, 0)
+            working_list.append(working_number)
+        else:
+            working_list.append(unassigned_vars[index])
 
-    return working_list
+    return Node(working_list, new_assigned_vars)
 
 
 class Sudoku(object):
@@ -110,43 +130,30 @@ class Sudoku(object):
     def solve(self):
         # self.ans is a list of lists
         # outer list is the row, inner list is col-wise
-        # global current_state
+        global current_state
+        stack = [init_solve(puzzle)]
 
-        global current_state, v
-        start_state = init_solve(puzzle)
-        stack = [start_state]
-        completed = False
-
-        while len(stack) > 0 and not completed:
+        while len(stack) > 0:
             current_state = stack.pop()
 
-            cull_state = False
-
-            # check for invalid domain, unassigned but domain is empty
-            # variables are ordered from smallest domain to max domain
-            # however, empty domains are given a length of maxint so they're pushed to the end
-            # this is why the algo checks from end to the start
-            for i in xrange(len(current_state.variables) - 1, -1, -1):
-                inspector_variable = current_state.variables[i]
-                # ensures that the scanning is only done for variables that have empty domain
-                if inspector_variable.domain_size < sys.maxint:
-                    break
-                elif inspector_variable.assigned_number == 0:
-                    cull_state = True
-                    break
-            if cull_state:
-                continue
-
-            if current_state.variables[0].domain_size < 10:
-                for n in current_state.variables[0].domain:
-                    current_state.variables[0].assign_number(n)
-                    unsorted_list = domain_update(current_state.variables)
-                    stack.append(Node(unsorted_list))
+            if len(current_state.variables) > 0:
+                if len(current_state.variables[0].domain) > 0:
+                    for n in current_state.variables[0].domain:
+                        stack.append(domain_update(current_state.variables, current_state.assigned_vars, n))
+                else:
+                    # no vars with valid domains left.
+                    # domain is valid when it is more than 0
+                    # continue to quit this state
+                    continue
             else:
+                # no variables left to perform assignment
                 break
 
-        for var in current_state.variables:
-            self.ans[var.row_num - 1][var.col_num - 1] = var.assigned_number
+        for var in current_state.assigned_vars.items():
+            row_num = (var[0] - 1) // 9
+            col_num = (var[0] - 1) % 9
+
+            self.ans[row_num][col_num] = var[1]
 
         return self.ans
 
